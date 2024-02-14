fb8b6ff3740659c78413e886aabbe0650462b3ca
Unnati Rastogi <rastogiunnati25@gmail.com>
2024-02-14 23:03:08 +0530

    added tailwind

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCV0RW1FtwAgebzKjBKk7chBwuMy6AMpeA",
+  authDomain: "vite-weather.firebaseapp.com",
+  projectId: "vite-weather",
+  storageBucket: "vite-weather.appspot.com",
+  messagingSenderId: "1037393654560",
+  appId: "1:1037393654560:web:b4a8f8609d418c0bb4dd45"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
